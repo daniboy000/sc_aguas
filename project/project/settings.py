63f9d618ffb3d7cb07f43bd23aa5ffff89cdd36b
Andrews Medina <andrewsmedina@gmail.com>
2016-06-28 22:43:06 -0300

    add rest api

--- project/project/settings.py
+++ project/project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'aguas',
 ]
 
@@ -121,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    )
+}
