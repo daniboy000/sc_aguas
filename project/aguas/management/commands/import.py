7b1c8fe981c7d9192ce6738a98105f797aebccc3
Andrews Medina <andrewsmedina@gmail.com>
2016-06-29 00:31:09 -0300

    remove unused code

--- project/aguas/management/commands/import.py
+++ project/aguas/management/commands/import.py
@@ -7,64 +7,6 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_municipios(soup):
-    """ returns a dict of municipios
-
-    Args:
-        soup (Beautifulsoup): the FATMA page content
-
-    Returns:
-        dict: a dict of municipios where the key is the municipio name and the
-        value is the municipio id
-
-    """
-
-    # get 'select' tags with id == 'combo_municipio'
-    combo_municipio = soup.find('select', id='combo_municipio')
-
-    # get all 'option' tags
-    combo_municipio_values = combo_municipio.find_all('option')
-
-    municipios_index = {}
-    for i in combo_municipio_values:
-        municipios_index[i.text.encode('utf-8')] = i['value']
-
-    return municipios_index
-
-
-def get_municipio_areas(id_municipio):
-    """ Returns a list of areas from a municipio
-
-    Args:
-        id_municipio (int): the municipio id at FATMA page
-
-    Returns:
-        dict: a dict of areas where the key is the area name and the value is
-        the area id
-
-    Note:
-        The URL to request the municipio areas
-
-        http://www.fatma.sc.gov.br/laboratorio/gravar.php?operacao=
-        selecionarBalnearios&oid=2
-    """
-    url = "http://www.fatma.sc.gov.br/laboratorio/gravar.php"
-    url += "?operacao=selecionarBalnearios&oid={}".format(id_municipio)
-
-    municipio_areas = {}
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        list_request = response.text.split('|')
-
-        for i in range(1, len(list_request) - 1, 2):
-            area = list_request[i+1]
-            area_code = list_request[i]
-            municipio_areas[area] = area_code
-
-    return municipio_areas
-
-
 def get_balneabilidade_for_area(municipio_name, municipio_id, area_id):
     """
     http://www.fatma.sc.gov.br/laboratorio/balneabilidade.php?municipio=FLORIANOPOLIS&m=2&b=77
@@ -113,16 +55,6 @@ class Command(BaseCommand):
     help = 'import data'
 
     def handle(self, *args, **options):
-        url = 'http://www.fatma.sc.gov.br/laboratorio/dlg_balneabilidade.php'
-
-        response = requests.get(url)
-        if response.status_code != 200:
-            return
-
-        page_content = response.text
-
-        soup = BeautifulSoup(page_content, 'html.parser')
-
         baln = get_balneabilidade_for_area('FLORIANOPOLIS', 2, 77)
 
         for i in baln:
